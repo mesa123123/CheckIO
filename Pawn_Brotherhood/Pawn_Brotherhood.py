f47da4486892be1a62e9aa0b19182174c56f3cfd
mesa123123 <bowmanpete123@gmail.com>
2018-02-24 14:37:47 +1300

    Added py.test for Pawn_Brotherhood but haven't got a decent answer as of yet

--- Pawn_Brotherhood/Pawn_Brotherhood.py
+++ Pawn_Brotherhood/Pawn_Brotherhood.py
@@ -1,2 +1,8 @@
+
 def safe_pawns(positions):
-    return 5
+    return len(set([protected_pawn for protected_pawn in positions for defending_pawn in positions
+                    if abs(ord(protected_pawn[0])- ord(defending_pawn[0])) == 1
+                    and int(protected_pawn[1]) - int(defending_pawn[1]) == 1 ]))
+
+
+
--- Pawn_Brotherhood/test_Pawn_Brotherhood.py
+++ Pawn_Brotherhood/test_Pawn_Brotherhood.py
@@ -1,7 +1,7 @@
 from Pawn_Brotherhood import safe_pawns
 
 def test_one():
-    assert(safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}) == 6)
+    assert(6 == safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}))
 
 def test_two():
-    assert(safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}) == 1)
+    assert(1 == safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}))
